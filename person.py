bc6e197012c4a0bd2311e3ee3b679576cfd48a1b
brwla <21-06187@g.batstate-u.edu.ph>
2022-11-25 23:52:10 +0800

    edited the codes

--- person.py
+++ person.py
@@ -1,58 +1,67 @@
 #person
 class Person:
-    def __init__(self,name,age,contact,address):
+    def __init__(self, name, age, contact, address):
         self.name = name 
         self.age =  age 
         self.contact = contact
         self.address = address
 
-    def printname (self):
-        print(self.name,self.age, self.contact, self.address)
+    def introduction(self):
+        print(f"Hello, I am {self.name},{self.age} years old.")
+        print(f"You can contact me on {self.contact}.")
+        print(f"I am currently living in {self.address}.")
 
-class Employee (Person):
-    def __init__(self,name,age,contact,address):
-        super().__init__(name,age,contact,address)
+#Employee
+class Employee(Person):
+    pass
 
-    def printname (self):
-        print(self.name, self.age, self.contact, self.address)
-
+#Doctor
 class Doctor(Employee):
-    def __init__(self,name,age,contact,address):
-        super().__init__(name,age,contact,address)
+    def __init__(self, name, age, contact, address):
+        super().__init__(name, age, contact, address)
 
-    def Checkup (self):
-        print(f"{self.name} is performing a check up on {Patientname}")
+    def checkUp (self, patientName):
+        print(f"{self.name} is performing a check up on {patientName}")
 
-#class Pharmacist(Employee):
-    #def __init__(self,name,age,contact,address):
-        #super().__init__(name,age,contact,address)
+    def giveMedication(self, patientName):
+        print(f"{self.name} is giving a presciption to {patientName}")
 
-    #def Checkup (self):
-        #print(f"{self.name} is performing a check up on {Patientname}")S
+#Assistant
+class Assistant(Employee):
+    def __init__(self, name, age, contact, address):
+        super().__init__(name, age, contact, address)
 
+    def recordPatientInfo(self, patientName):
+        print(f"{self.name} is recording {patientName}'s medical record.")
 
-employee = Person ("Dr. Riza Villanueva", 45, "092769587063", "Batangas Rizal Avenue")
-employee.printname()
-
-class Patient (Person):
-    def __init__(self,name,age,contact,address):
-        super().__init__(name,age,contact,address)
-
-    def printname (self):
-        print(self.name, self.age, self.contact, self.address)
-
-Patientname = input("Enter your name: ")
-Patientage = input("Enter your age: ")
-Patientcontact = input("Enter your contact: ")
-Patientaddress = input("Enter your address: ")
-
-kiara = Patient(Patientname,Patientage,Patientcontact,Patientaddress)
-kiara.printname()
-#employee = Person ("Riza", "Villanueva", 45, "092769587063", "Batangas Rizal Avenue")
-#employee.printname()
-
-doktor = Doctor("a", 1, 1, "a")
-doktor.Checkup()
-
-
+    def schedAppointment(self, patientName):
+        print(f"{self.name} is scheduling an appointment for {patientName}")
     
+
+#Pharmacist
+class Pharmacist(Employee):
+    def __init__(self, name, age, contact, address):
+        super().__init__(name, age, contact, address)
+
+    def readPrescription(self, doctorName):
+        print(f"{self.name} is reading the Dr. {doctorName}'s prescription.")
+
+    def giveMedicine(self, patientName):
+        print(f"{self.name} is giving the medicine to {patientName}")
+
+#Patient
+class Patient (Person):
+    def __init__(self, name, age, contact, address):
+        super().__init__(name, age, contact, address)
+
+    def introduction(self):
+        super().introduction()
+
+    def makeAppointment(self):
+        print(f"{self.name} is making an appointment for a checkup.")
+
+    def buyMedicine(self):
+        print(f"{self.name} is buying a medicine.")
+
+    def pay(self, payment):
+        print(f"{self.name} is paying for {payment}")
