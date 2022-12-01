43e750946470bd3516f16427720976a525276f0e
brwla <21-06187@g.batstate-u.edu.ph>
2022-12-01 13:03:42 +0800

    Revised the codes

---
+++ doctor_information.py
@@ -0,0 +1,99 @@
+import os
+import json
+
+class Doctor:
+    def __init__(self):
+        if os.path.exists('doctor_records.json'): pass #this checks if the file exists
+        else:    #if not it will create a new file
+            f = open('doctor_records.json','x')
+            f.close()
+        self.doctor_records = {} #dictionary
+
+    def load_from_file(self):   #loads the data on the file to the dict self.doctor_records
+        if os.stat('doctor_records.json').st_size == 0:  #checks if the file is empty
+            pass
+        else:  #if not it will load the information from the file to the dictionary
+            with open('doctor_records.json') as f:
+                self.doctor_records = json.load(f)
+            f.close()
+
+    def write_to_file(self):  #writing the dictionary to the json file
+        with open('doctor_records.json', 'w') as f:  #writing to file
+            json.dump(self.doctor_records, f, indent=4)
+        f.close()
+
+    def add_doctor(self):   #add doctor info
+        self.load_from_file()
+        try:
+            doc_id = input("\t\tEnter the doctor's id: ")
+            if doc_id in self.doctor_records:   #checks if the id already exists
+                print("\t\tID already exists.")
+                input("\t\tPress enter to continue.")
+                return
+            else:   #if the id doesn't exists it will execute this block of code
+                doctor_info = {}
+                doctor_info["Name"] = input("\t\tWhat is the doctor's name? ")
+                doctor_info["Age"] = input("\t\tWhat is the doctor's age? ")
+                doctor_info["Gender"] = input("\t\tWhat is the doctor's gender? ")
+                doctor_info["Address"] = input("\t\tWhat is the doctor's address? ")
+                self.doctor_records[doc_id] = doctor_info  #putting the doctor_info dictionary inside the doctor_records dictionary
+        except:
+            print("\t\tAn error occurred.")
+        self.write_to_file()
+
+    def update_doctor(self):   #update doctor info
+        self.load_from_file()
+        self.show_doc_info()
+        try:  #checks if the doctor id exists
+            update = input("\t\tChoose the doctor id that you want to modify: ")
+            self.doctor_records[update] 
+        except KeyError:  #if not it will print out the error message
+            print("\t\tDoctor's information not found.")
+            input("\t\tPress enter to continue.")
+        else:   #this will execute if the try block executed
+            self.doctor_records[update]["Name"] = input("\t\tWhat will be the updated name? ")
+            self.doctor_records[update]["Age"] = input("\t\tWhat will be the updated age? ")
+            self.doctor_records[update]["Gender"] = input("\t\tWhat will be the updated gender? ")
+            self.doctor_records[update]["Address"] = input("\t\tWhat will be the updated address? ")
+            self.write_to_file()
+
+    def show_doc_info(self):  #display informations
+        self.load_from_file()
+        print('-'*70)
+        print("\t\t-Doctor Records-")
+        print(f"\t\tCurrent number of Doctors:{len(self.doctor_records)}")
+        print('-'*70)
+        for doc_id, doc_info in self.doctor_records.items():
+            print("\t\tDoctor ID:", doc_id)
+            for key in doc_info:
+                print(f"\t\t{key}: {doc_info[key]}")
+            print('-'*70)
+
+    def delete_doctor(self):   #delete doctor info
+        self.load_from_file()
+        self.show_doc_info()
+        try:  #checks if the id exists
+            delete = input("\t\tChoose the doctor id that you want to delete: ")
+            self.doctor_records[delete]
+        except KeyError:   #if not it will print out the error message
+            print("\t\tDoctor's information not found.")
+            input("\t\tPress enter to continue.")
+        else:   #this will execute if the try block executed
+            del self.doctor_records[delete]
+
+            self.write_to_file()
+
+    def search_doctor(self):   #search doctor info
+        self.load_from_file()
+        try:   #checks if the id exists
+            search = input("\t\tSearch Doctor's ID: ")
+            self.doctor_records[search]
+        except KeyError:  #if the try block fails the except will catch the error
+            print(f"\t\t{search} does not exist")
+            input("\t\tPress enter to continue.")
+        else:   #this will execute if the try block executed
+            print('-'*70)
+            for key, value in self.doctor_records[search].items():
+                print(f"\t\t{key}: {value}")
+            print('-'*70)
+            input("\t\tPress enter to continue.")
--- menu.py
+++ menu.py
@@ -1,14 +1,199 @@
-from drug_store import *
-from clinic import *
+from doctor_information import *
 
-#This is only for testing the code
+class Menu:
+    def __init__(self):
+        self.doc=Doctor()
+    
+    def main_menu(self):
+        while True:
+            print ('-'*70)
+            print("\n\n\t\tWelcome to Clinic Management System\n\n")
+            print ('-'*70)
+            print("\n\n\t\t[1] Admin Menu")
+            print("\n\t\t[2] Patient Menu")
+            print("\n\t\t[3] Exit")
+            choice = input("\n\n\t\tWhere do you want to go? ")
+            if choice == '1':
+                self.admin_menu()
+            elif choice == '2':
+                self.patient_menu()
+            elif choice == '3':
+                exit()
+            else:
+                print("\n\n\t\tInvalid choice.") 
 
-c = Clinic()
-c.introduce()
-c.add_appointment()
-c.checkup_with_prescription()
-c.pay_checkup()
-c.finished_appointment()
+    def admin_menu(self):
+        while True:
+            print ('-'*70)
+            print("\n\n\t\t-----Admin Menu-----\n\n")
+            print ('-'*70)
+            print("\n\n\t\t[1] Patient Records")
+            print("\n\t\t[2] Doctor's Informations")
+            print("\n\t\t[3] Medicine List")
+            print("\n\t\t[4] Appointment List")
+            print("\n\t\t[5] Go back to Main Menu")
+            print("\n\t\t[6] Exit")
+            choice = input("\n\n\t\tWhere do you want to go? ")
+            if choice == '1':
+                self.patient_records_menu()
+            elif choice == '2':
+                self.doctor_information_menu()
+            elif choice == '3':
+                self.medicine_menu()
+            elif choice == '4':
+                self.appointment_menu()
+            elif choice == '5':
+                return
+            elif choice == '6':
+                exit()
+            else:
+                print("\n\n\t\tInvalid choice.") 
 
-d = DrugStore()
-d.start()
+    def patient_menu(self):
+        while True:
+            print ('-'*70)
+            print("\n\n\t\t-----Patient Menu-----\n\n")
+            print ('-'*70)
+            print("\n\n\t\t[1] Make an Appointment")
+            print("\n\t\t[2] Buy Medicine")
+            print("\n\t\t[3] Go back to Main Menu")
+            print("\n\t\t[4] Exit")
+            choice = input("\n\n\t\tWhere do you want to go? ")
+            if choice == '1':
+                pass    #put the method for adding an appointment
+            elif choice == '2':
+                pass    #put the method for buying medicine
+            elif choice == '3':
+                return
+            elif choice == '4':
+                exit()
+            else:
+                print("\n\n\t\tInvalid choice.") 
+
+    def patient_records_menu(self):
+        while True:
+            print ('-'*70)
+            print("\n\n\t\t-----Patient Records Menu-----\n\n")
+            print ('-'*70)
+            print("\n\n\t\t[1] Add new Patient Record")
+            print("\n\t\t[2] Search Patient Record")
+            print("\n\t\t[3] View Patient Records")
+            print("\n\t\t[4] Modify Patient Record")
+            print("\n\t\t[5] Delete Patient Record")
+            print("\n\t\t[6] Go back to Admin Menu")
+            print("\n\t\t[7] Exit")
+            choice = input("\n\n\t\tWhere do you want to go? ")
+            if choice == '1':
+                pass    #add
+            elif choice == '2':
+                pass    #search
+            elif choice == '3':
+                pass    #view/display/show
+            elif choice == '4':
+                pass    #update/modify
+            elif choice == '5':
+                pass    #delete
+            elif choice == '6':
+                return
+            elif choice == '7':
+                exit()
+            else:
+                print("\n\n\t\tInvalid choice.") 
+
+    def doctor_information_menu(self):
+        while True:
+            print ('-'*70)
+            print("\n\n\t\t-----Doctor Information Menu-----\n\n")
+            print ('-'*70)
+            print("\n\n\t\t[1] Add new Doctor")
+            print("\n\t\t[2] Search a Doctor")
+            print("\n\t\t[3] View Doctor lists")
+            print("\n\t\t[4] Modify Doctor's Information")
+            print("\n\t\t[5] Delete Doctor's Information")
+            print("\n\t\t[6] Go back to Admin Menu")
+            print("\n\t\t[7] Exit")
+            choice = input("\n\n\t\tWhere do you want to go? ")
+            if choice == '1':
+                self.doc.add_doctor()
+            elif choice == '2':
+                self.doc.search_doctor()
+            elif choice == '3':
+                self.doc.show_doc_info()
+                input("\t\tPress enter to continue.")
+            elif choice == '4':
+                self.doc.update_doctor()
+            elif choice == '5':
+                self.doc.delete_doctor()
+            elif choice == '6':
+                return
+            elif choice == '7':
+                exit()
+            else:
+                print("\n\n\t\tInvalid choice.") 
+
+    def medicine_menu(self):
+        while True:
+            print ('-'*70)
+            print("\n\n\t\t-----Medicine Menu-----\n\n")
+            print ('-'*70)
+            print("\n\n\t\t[1] Add new Medicine")
+            print("\n\t\t[2] Search a Medicine")
+            print("\n\t\t[3] View Medicine lists")
+            print("\n\t\t[4] Modify Medicine Information")
+            print("\n\t\t[5] Delete Medicine Information")
+            print("\n\t\t[6] Go back to Admin Menu")
+            print("\n\t\t[7] Exit")
+            choice = input("\n\n\t\tWhere do you want to go? ")
+            if choice == '1':
+                pass    #add
+            elif choice == '2':
+                pass    #search
+            elif choice == '3':
+                pass    #view/display/show
+            elif choice == '4':
+                pass    #update/modify
+            elif choice == '5':
+                pass    #delete
+            elif choice == '6':
+                return
+            elif choice == '7':
+                exit()
+            else:
+                print("\n\n\t\tInvalid choice.") 
+
+    def appointment_menu(self):
+        while True:
+            print ('-'*70)
+            print("\n\n\t\t-----Appointment Menu-----\n\n")
+            print ('-'*70)
+            print("\n\n\t\t[1] Add new Appointment")
+            print("\n\t\t[2] Search an Appointment")
+            print("\n\t\t[3] View Appointment lists")
+            print("\n\t\t[4] Modify an Appointment")
+            print("\n\t\t[5] Delete an Appointment")
+            print("\n\t\t[6] Go back to Admin Menu")
+            print("\n\t\t[7] Exit")
+            choice = input("\n\n\t\tWhere do you want to go? ")
+            if choice == '1':
+                pass    #add
+            elif choice == '2':
+                pass    #search
+            elif choice == '3':
+                pass    #view/display/show
+            elif choice == '4':
+                pass    #update/modify
+            elif choice == '5':
+                pass    #delete
+            elif choice == '6':
+                return
+            elif choice == '7':
+                exit()
+            else:
+                print("\n\n\t\tInvalid choice.") 
+
+
+
+
+#driver
+start=Menu()
+start.main_menu()
